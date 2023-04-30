e2f0fb3bd7887dffe5ad9ebd41f61f359ac3eb73
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-04-30 20:07:33 +0700

    fix: Laporan Penjualan

--- src/main/java/UI/Page/LaporanPenjualan.java
+++ src/main/java/UI/Page/LaporanPenjualan.java
@@ -1,6 +1,6 @@
 package UI.Page;
 import javax.swing.*;
-
+import javax.swing.border.Border;
 import javax.swing.*;
 import java.awt.*;
 
@@ -23,11 +23,13 @@ public class LaporanPenjualan extends JFrame {
         // tambahkan judul page ke panel kiri
         JLabel judulLabel = new JLabel("Laporan Penjualan");
         judulLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        judulLabel.setBorder(BorderFactory.createEmptyBorder(80, 100, 0, 0));
         kiriPanel.add(judulLabel, BorderLayout.NORTH);
 
         // tambahkan detail penjualan ke panel kiri
         JPanel detailPanel = new JPanel();
-        detailPanel.setLayout(new GridLayout(0, 3));
+        detailPanel.setBorder((BorderFactory.createEmptyBorder(20, 20, 0, 0)));
+        detailPanel.setLayout(new GridLayout(0, 3, 60, 10));
         detailPanel.add(new JLabel("Nama Item"));
         detailPanel.add(new JLabel("Jumlah"));
         detailPanel.add(new JLabel("Harga"));
@@ -42,35 +44,44 @@ public class LaporanPenjualan extends JFrame {
         JScrollPane scrollPane = new JScrollPane(detailPanel);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        kiriPanel.add(scrollPane, BorderLayout.CENTER);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder(20, 83, 0, 0));
+        kiriPanel.add(scrollPane);
 
         // tambahkan panel kiri ke dalam frame
         add(kiriPanel, BorderLayout.WEST);
 
         // tambahkan komponen ke panel kanan
         JPanel kananPanel = new JPanel();
-        kananPanel.setLayout(new BorderLayout());
+        kananPanel.setLayout(new BoxLayout(kananPanel, BoxLayout.Y_AXIS));
 
         // tambahkan label Total Pemasukan ke panel kanan
         JLabel totalLabel = new JLabel("Total Pemasukan:");
+        totalLabel.setBorder(BorderFactory.createEmptyBorder(135, 80, 5, 480));
         kananPanel.add(totalLabel, BorderLayout.NORTH);
 
         // tambahkan jumlah total harga ke panel kanan
         JLabel hargaLabel = new JLabel("Rp24000");
-        kananPanel.add(hargaLabel, BorderLayout.CENTER);
+        hargaLabel.setBorder(BorderFactory.createEmptyBorder(5, 80, 5, 480));
+        kananPanel.add(hargaLabel);
 
         // tambahkan tombol print ke panel kanan
         JButton printButton = new JButton("Print Laporan Penjualan");
+        printButton.setBorder(BorderFactory.createEmptyBorder(5, 80, 5, 80));
+        printButton.setPreferredSize(new Dimension(150, 30));
         kananPanel.add(printButton, BorderLayout.SOUTH);
 
         // tambahkan panel kanan ke dalam frame
         add(kananPanel, BorderLayout.EAST);
 
         // tampilkan frame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setSize(1200, 720);
+        setLocationRelativeTo(null);
         setVisible(true);
     }
 
-    // public static void main(String[] args) {
-    //     LaporanPenjualan lapPenjualan = new LaporanPenjualan();
-    // }
+    public static void main(String[] args) {
+        LaporanPenjualan lapPenjualan = new LaporanPenjualan();
+    }
 }
