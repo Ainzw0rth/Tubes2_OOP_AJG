93ec15af333d54a6d81424182ae25d9396434cff
louis <louiscaesapdf@gmail.com>
2023-05-07 23:33:59 +0700

    add printer to jualbarang

--- src/main/java/UI/Page/JualBarang.java
+++ src/main/java/UI/Page/JualBarang.java
@@ -1,21 +1,32 @@
 package UI.Page;
 import javax.swing.*;
 
+import com.itextpdf.text.DocumentException;
+
 import DataStore.DataStore;
 import Entity.Bill;
 import Entity.Item;
 import Entity.Member;
 import UI.App;
 import UI.IApp;
+import Utils.Printer;
 import Utils.Collections.Observer;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.LinkedList;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class JualBarang extends JPanel {
     private JScrollPane scrollPane;
-    JPanel stockPanel;
+    private JPanel stockPanel;
     private JPanel itemPanel;
     private JScrollPane stockScrollPane;
     private Bill x;
@@ -23,9 +34,16 @@ public class JualBarang extends JPanel {
 
     private int minprice = 0;
     private int maxprice = 999999999;
+    private String itemName = "";
 
     
     private JLabel subtotalLabelNumber;
+
+    private JComboBox<String> memberDropdown;
+    private JComboBox<String> kategori;
+
+    private String printableBill = "";
+    private Printer printer = new Printer();
 
     public JualBarang() {
         try {
@@ -53,7 +71,13 @@ public class JualBarang extends JPanel {
                 public void actionPerformed(ActionEvent e) {
                     String inp = minPriceLabelNumber.getText();
                     try {
-                        int price = Integer.parseInt(inp);
+                        int price;
+                        if (inp.equals("")) {
+                            price = 0;
+                        } else {
+                            price = Integer.parseInt(inp);
+                        }
+
                         minprice = price;
                         rerenderItems();
                     } catch (Exception a) {
@@ -75,7 +99,12 @@ public class JualBarang extends JPanel {
                 public void actionPerformed(ActionEvent e) {
                     String inp = maxPriceLabelNumber.getText();
                     try {
-                        int price = Integer.parseInt(inp);
+                        int price;
+                        if (inp.equals("")) {
+                            price = 999999999;
+                        } else {
+                            price = Integer.parseInt(inp);
+                        }
                         maxprice = price;
                         rerenderItems();
                     } catch (Exception a) {
@@ -84,13 +113,38 @@ public class JualBarang extends JPanel {
             });
             
             // category dropdown
-            String[] categories = {"Category"};
-            JComboBox<String> kategori = new JComboBox<>(categories);
+            
+            LinkedList <String> categories = new LinkedList<String>();
+            categories.add("Category");
+
+            for (Item x: d.getItems().getElements()) {
+                if (!categories.contains(x.getCategory())) {
+                    categories.add(x.getCategory());
+                }
+            }
+            
+            DefaultComboBoxModel<String> basecategoriesmodel = new DefaultComboBoxModel<>(categories.toArray(new String[0]));
+            kategori = new JComboBox<>(basecategoriesmodel);
             kategori.setBounds(620, 10, 150, 30);
+            kategori.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    rerenderItems();
+                }
+                
+            });
     
             // input searching
             JTextField searchField = new JTextField();
             searchField.setBounds(20, 10, 150, 30);
+            searchField.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String inp = searchField.getText();
+                    itemName = inp;
+                    rerenderItems();
+                }
+            });
     
             filterPanel.add(searchField);
             filterPanel.add(minPriceLabel);
@@ -120,9 +174,18 @@ public class JualBarang extends JPanel {
             });
     
             // member dropdown
-            String[] items = {"Pilih nama member"};
-            JComboBox<String> dropdown = new JComboBox<>(items);
-            dropdown.setBounds(15, 420, 150, 30);
+            // String[] items = {"Pilih nama member"};
+
+            LinkedList <String> members = new LinkedList<String>();
+            members.add("Pilih nama member");
+
+            for (Member x: d.getActiveMembers()) {
+                members.add(x.getName());
+            }
+
+            DefaultComboBoxModel<String> basemembermodel = new DefaultComboBoxModel<>(members.toArray(new String[0]));
+            memberDropdown = new JComboBox<>(basemembermodel);
+            memberDropdown.setBounds(15, 420, 150, 30);
     
             // subtotal label
             JLabel subtotalLabel = new JLabel("Subtotal");
@@ -169,6 +232,16 @@ public class JualBarang extends JPanel {
             printBillButton.setForeground(Color.black);
             printBillButton.setBounds(199, 470,199, 48);
             printBillButton.setBackground(new Color(0xEBEBEB));
+            printBillButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        printer.printComponentToPdf(printableBill);
+                    } catch (DocumentException e1) {
+                    }
+                }
+            });
+
             // STOCK PANEL
             stockPanel = new JPanel();
             stockPanel.setLayout(new FlowLayout());
@@ -186,7 +259,7 @@ public class JualBarang extends JPanel {
             itemPanel = new JPanel();
             scrollPane = new JScrollPane(itemPanel);
             scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-            scrollPane.setBounds(15, 60, 370, 270);
+            scrollPane.setBounds(10, 60, 370, 270);
             scrollPane.setBackground(Color.white);
             scrollPane.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, new Color(0x36459A)));
             rerenderBills();
@@ -208,7 +281,7 @@ public class JualBarang extends JPanel {
             panel.add(saveBillButton);
             panel.add(printBillButton);
             panel.add(billLabel);
-            panel.add(dropdown);
+            panel.add(memberDropdown);
     
             // frame pagenya
             // JFrame frame = new JFrame();
@@ -245,9 +318,17 @@ public class JualBarang extends JPanel {
         int ctr = 0;
         
         for (Item item: d.getItems().getElements()) {
-            if (item.getPrice() <= maxprice && item.getPrice() >= minprice) {
+            String cat = (String) this.kategori.getSelectedItem();
+            if (((!cat.equals("Category") && cat.equals(item.getCategory())) || cat.equals("Category")) && (itemName.equals("") || (!itemName.equals("") && itemName.equals(item.getName()))) && item.getPrice() <= maxprice && item.getPrice() >= minprice) {
                 ctr++;
-                ImageIcon image1 = new ImageIcon(item.getImageUrl()); // path nanti diganti dengan image yang sesuai
+                ImageIcon image1 = new ImageIcon(getClass().getResource("/images/icon.jpg"));
+                
+                try {
+                    BufferedImage image = ImageIO.read(new File(item.getImageUrl())); // untuk mengecek file ada atau tidak
+                    image1 = new ImageIcon(item.getImageUrl());
+                } catch (IOException e) {
+                }
+                
                 Image scaledImage = image1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
                 image1 = new ImageIcon(scaledImage);
                 JButton button1 = new JButton(image1);
@@ -267,102 +348,110 @@ public class JualBarang extends JPanel {
 
         ctr = ctr / 5;            
 
-        Dimension panelSize = new Dimension(802, (ctr+1)*116);
+        Dimension panelSize = new Dimension(750, (ctr+1)*116);
         stockPanel.setPreferredSize(panelSize);
         stockScrollPane.setViewportView(stockPanel);
     }
 
     public void rerenderBills () {
+        // keperluan print
+        String tab = "    ";
+
         itemPanel = new JPanel();
         itemPanel.setLayout(new BoxLayout(itemPanel, BoxLayout.Y_AXIS));
         itemPanel.setBackground(Color.white);
 
         int ctr = 0;
+        printableBill = "Bill:\n\nBill ID                 : ";
+        printableBill += Integer.toString(x.getId()) + "\nCustomer ID      : " + Integer.toString(x.getIdCustomer()) + "\nBarang-barang   : \n";
         for (Item item : x.getItems()) {
-            if (item.getPrice() <= maxprice && item.getPrice() >= minprice) { 
-                ctr++;
-                JPanel panelItem = new JPanel();
-                panelItem.setBackground(Color.white);
-                panelItem.setMaximumSize(new Dimension(1000, 90));
-    
-                JPanel itemNames = new JPanel();
-                itemNames.setBounds(15, 0, 120, 20);
-                itemNames.setBackground(Color.white);
-                itemNames.setLayout(null);
-                
-                JPanel itemPrices = new JPanel();
-                itemPrices.setBounds(210, 0, 120, 20);
-                itemPrices.setBackground(Color.white);
-                itemPrices.setLayout(null);
-                
-                JLabel nameLabel = new JLabel(item.getName());
-                nameLabel.setHorizontalAlignment(JLabel.LEFT); // align text to left
-                nameLabel.setBounds(0, 0, 120, 20);
-                itemNames.add(nameLabel, BorderLayout.WEST); // add name label to left side of panel
-    
-                JLabel priceLabel = new JLabel("Rp " + item.getPrice());
-                priceLabel.setHorizontalAlignment(JLabel.LEFT); // align text to right
-                priceLabel.setBounds(0, 0, 120, 20);
-                itemPrices.add(priceLabel, BorderLayout.EAST);
-    
-                // tombol untuk menambah/mengurangi stok
-                JPanel addminPanel = new JPanel();
-                addminPanel.setLayout(null);
-                addminPanel.setBounds(200, 30, 130, 20);
-                addminPanel.setBackground(new Color(0xD9D9D9));
-    
-                JButton min = new JButton("-");
-                min.setFont(new Font("Poppins", Font.PLAIN, 10));
-                min.setBackground(new Color(0xEDEDED));
-                min.setLayout(null);
-                min.setBounds(0, 0, 40, 20);
-                min.setFocusPainted(false);
-                min.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        x.hapus(item);
-                        rerenderBills();
-                    }
-                });
-    
-                JButton plus = new JButton("+");
-                plus.setFont(new Font("Poppins", Font.PLAIN, 10));
-                plus.setBackground(new Color(0xEDEDED));
-                plus.setLayout(null);
-                plus.setBounds(90, 0, 40, 20);
-                plus.setFocusPainted(false);
-                plus.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        x.tambah(item);
-                        rerenderBills();
-                    }
-                });
-    
-                JPanel amountPanel = new JPanel(new GridBagLayout());
-                amountPanel.setBounds(40, 0, 50, 20);
-    
-                GridBagConstraints gbc = new GridBagConstraints();
-                gbc.gridx = 0;
-                gbc.gridy = 0;
-                gbc.fill = GridBagConstraints.BOTH;
-                gbc.anchor = GridBagConstraints.CENTER;
-    
-                JLabel amountText = new JLabel(Integer.toString(item.getStock()));
-    
-                amountPanel.add(amountText, gbc);
-                
-                addminPanel.add(min);
-                addminPanel.add(plus);
-                addminPanel.add(amountPanel);
-                
-                panelItem.add(itemNames);
-                panelItem.add(itemPrices);
-                panelItem.add(addminPanel);
-                panelItem.setLayout(null);
-                itemPanel.add(panelItem);
-            }
+            ctr++;
+            JPanel panelItem = new JPanel();
+            panelItem.setBackground(Color.white);
+            panelItem.setMaximumSize(new Dimension(1000, 90));
+
+            JPanel itemNames = new JPanel();
+            itemNames.setBounds(15, 0, 120, 20);
+            itemNames.setBackground(Color.white);
+            itemNames.setLayout(null);
+            
+            JPanel itemPrices = new JPanel();
+            itemPrices.setBounds(210, 0, 120, 20);
+            itemPrices.setBackground(Color.white);
+            itemPrices.setLayout(null);
+            
+            JLabel nameLabel = new JLabel(item.getName());
+            nameLabel.setHorizontalAlignment(JLabel.LEFT); // align text to left
+            nameLabel.setBounds(0, 0, 120, 20);
+            itemNames.add(nameLabel, BorderLayout.WEST); // add name label to left side of panel
+
+            JLabel priceLabel = new JLabel("Rp " + item.getPrice());
+            priceLabel.setHorizontalAlignment(JLabel.LEFT); // align text to right
+            priceLabel.setBounds(0, 0, 120, 20);
+            itemPrices.add(priceLabel, BorderLayout.EAST);
+
+            // tombol untuk menambah/mengurangi stok
+            JPanel addminPanel = new JPanel();
+            addminPanel.setLayout(null);
+            addminPanel.setBounds(200, 30, 130, 20);
+            addminPanel.setBackground(new Color(0xD9D9D9));
+
+            JButton min = new JButton("-");
+            min.setFont(new Font("Poppins", Font.PLAIN, 10));
+            min.setBackground(new Color(0xEDEDED));
+            min.setLayout(null);
+            min.setBounds(0, 0, 40, 20);
+            min.setFocusPainted(false);
+            min.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    x.hapus(item);
+                    rerenderBills();
+                }
+            });
+
+            JButton plus = new JButton("+");
+            plus.setFont(new Font("Poppins", Font.PLAIN, 10));
+            plus.setBackground(new Color(0xEDEDED));
+            plus.setLayout(null);
+            plus.setBounds(90, 0, 40, 20);
+            plus.setFocusPainted(false);
+            plus.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    x.tambah(item);
+                    rerenderBills();
+                }
+            });
+
+            JPanel amountPanel = new JPanel(new GridBagLayout());
+            amountPanel.setBounds(40, 0, 50, 20);
+
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.fill = GridBagConstraints.BOTH;
+            gbc.anchor = GridBagConstraints.CENTER;
+
+            JLabel amountText = new JLabel(Integer.toString(item.getStock()));
+
+            amountPanel.add(amountText, gbc);
+            
+            addminPanel.add(min);
+            addminPanel.add(plus);
+            addminPanel.add(amountPanel);
+            
+            panelItem.add(itemNames);
+            panelItem.add(itemPrices);
+            panelItem.add(addminPanel);
+            panelItem.setLayout(null);
+            itemPanel.add(panelItem);
+            
+            // PRINT
+            printableBill += tab + "- " + item.getName() + " x" + item.getStock() + " (Rp " + item.getPrice() + ")\n";
         }
+        printableBill += "Subtotal: Rp " + Integer.toString(x.getTotalPrice());
+        
 
         Dimension billPanelSize = new Dimension(350, (ctr)*70);
         itemPanel.setPreferredSize(billPanelSize);
@@ -372,9 +461,13 @@ public class JualBarang extends JPanel {
     }
 
     public void checkoutItems() {
-        // TODO : bikin page transaksi berhasil
-        IApp app = App.getInstance();
-        app.addTab("Transaksi Berhasil", new JualBarang());
+        try {
+            Integer custID = d.generateCustomerId();
+            String selectedMember = (String) memberDropdown.getSelectedItem();
+            IApp app = App.getInstance();
+            app.addTab("Transaksi Berhasil", new PembayaranBerhasil(selectedMember));
+        } catch (Exception e) {
+        }
     }
 
     public static void main(String[] args) throws Exception {  
